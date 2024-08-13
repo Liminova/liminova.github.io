00e6a76ecb4f09792535f2c093ea546a7f99ad99
Restyled.io <commits@restyled.io>
2024-08-13 02:48:36 +0000

    Restyled by clang-format

--- eslint.config.js
+++ eslint.config.js
@@ -4,15 +4,22 @@ import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {ignores: [".vitepress/cache/**/*.*", ".vitepress/theme/env.d.ts", "postcss.config.cjs"]},
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores : [
+      ".vitepress/cache/**/*.*", ".vitepress/theme/env.d.ts",
+      "postcss.config.cjs"
+    ]
+  },
+  {files : [ "**/*.{js,mjs,cjs,ts,vue}" ]},
+  {languageOptions : {globals : globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...tailwind.configs["flat/recommended"],
   ...pluginVue.configs["flat/essential"],
-  {rules: {"vue/multi-word-component-names": "off"}},
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {rules : {"vue/multi-word-component-names" : "off"}},
+  {
+    files : [ "**/*.vue" ],
+    languageOptions : {parserOptions : {parser : tseslint.parser}}
+  },
 ];
