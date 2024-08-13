dc705168ea95159a7e7eca9f19da67c5d006de3d
Kien Nguyen <kiennguyen19323@gmail.com>
2024-08-13 09:48:30 +0700

    chore: add `eslint` w/ plugins for `vue` and `tailwindcss` (#2)
    * chore(.gitignore): add `.eslintcache`
    
    * build(package.json): eslint, vue & tailwindcss plugins
    
    * chore(package.json): `lint` script
    
    * chore: `eslint.config.js`
    
    * refactor(theme/yanami/utils.ts): add return type
    
    * refactor(Blog, Contests): add `v-key`; sort classname; rm `@apply` from class

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import pluginJs from "@eslint/js";
+import tailwind from "eslint-plugin-tailwindcss";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {ignores: [".vitepress/cache/**/*.*", ".vitepress/theme/env.d.ts", "postcss.config.cjs"]},
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tailwind.configs["flat/recommended"],
+  ...pluginVue.configs["flat/essential"],
+  {rules: {"vue/multi-word-component-names": "off"}},
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+];
