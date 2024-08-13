ecd0160917a7eb2b76ab26ea29ccb57f80ac4512
Kien Nguyen <kiennguyen19323@gmail.com>
2024-08-13 14:56:42 +0700

    refactor(*.vue): remove `lang="postcss"`, destructure `page.frontmatter.value` (#4)
    * refactor(*.vue): rm `lang="postcss"` from style tags
    
    it's optional, vue handles it internally
    
    * refactor(CustomLayout.vue): add `scoped` to layout's style
    
    * refactor(CustomLayout.vue): destructure `date` from `page.frontmatter.value`

--- .vitepress/theme/yanami/layouts/CustomLayout.vue
+++ .vitepress/theme/yanami/layouts/CustomLayout.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import DefaultTheme from "vitepress/theme";
 import { useData } from "vitepress";
+import DefaultTheme from "vitepress/theme";
 import { formatDate } from "../utils";
 
 const { Layout } = DefaultTheme;
 const page = useData();
-const date = formatDate(page.frontmatter.value["date"]);
+const { title, author, date } = page.frontmatter.value;
 </script>
 
 <template>
     <Layout>
         <template class="my-4" #doc-before>
-            <h1 class="my-2 leading-10 text-4xl font-semibold">{{ $frontmatter.title }}</h1>
+            <h1 class="my-2 leading-10 text-4xl font-semibold">{{ title }}</h1>
             <h2 class="my-2">
-                {{ date.string }} •
+                {{ formatDate(date).string }} •
                 <span>
-                    Written by <span class="font-semibold">{{ $frontmatter.author }}</span>
+                    Written by <span class="font-semibold">{{ author }}</span>
                 </span>
             </h2>
         </template>
     </Layout>
 </template>
 
-<style lang="postcss">
+<style scoped>
 .image-wrapper {
     @apply flex flex-wrap justify-evenly gap-y-2;
 }
