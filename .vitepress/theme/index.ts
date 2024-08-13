987c9019873397a9e64d5ca3baedfa3ca542bfb1
j1nxie <rylieeeeexd@gmail.com>
2024-08-13 11:41:53 +0700

    feat: custom doc layout

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,13 @@
 import type { Theme, EnhanceAppContext } from "vitepress";
 import Blog from "./yanami/layouts/Blog.vue";
 import Contests from "./yanami/layouts/Contests.vue";
+import CustomLayout from "./yanami/layouts/CustomLayout.vue";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 
 export default {
     extends: DefaultTheme,
+    Layout: CustomLayout,
     enhanceApp({ app }: EnhanceAppContext) {
         app.component("Blog", Blog);
         app.component("Contests", Contests);
---
+++ .vitepress/theme/yanami/layouts/CustomLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DefaultTheme from "vitepress/theme";
+import { useData } from "vitepress";
+import { formatDate } from "../utils";
+
+const { Layout } = DefaultTheme;
+const page = useData();
+const date = formatDate(page.frontmatter.value["date"]);
+</script>
+
+<template>
+    <Layout>
+        <template class="my-4" #doc-before>
+            <h1 class="my-2 leading-10 text-4xl font-semibold">{{ $frontmatter.title }}</h1>
+            <h2 class="my-2">
+                {{ date.string }} •
+                <span>
+                    Written by <span class="font-semibold">{{ $frontmatter.author }}</span>
+                </span>
+            </h2>
+        </template>
+    </Layout>
+</template>
