f3f62f76e78ed0f92945c97c554da85fc1f6c88c
Delnegend <kiennguyen19323@gmail.com>
2024-08-13 14:37:23 +0700

    refactor(CustomLayout.vue): add `scoped` to layout's style

--- .vitepress/theme/yanami/layouts/CustomLayout.vue
+++ .vitepress/theme/yanami/layouts/CustomLayout.vue
@@ -1,28 +1,29 @@
 <script setup lang="ts">
-import DefaultTheme from "vitepress/theme";
 import { useData } from "vitepress";
+import DefaultTheme from "vitepress/theme";
 import { formatDate } from "../utils";
 
 const { Layout } = DefaultTheme;
 const page = useData();
 const date = formatDate(page.frontmatter.value["date"]);
+const { title, author } = page.frontmatter.value;
 </script>
 
 <template>
     <Layout>
         <template class="my-4" #doc-before>
-            <h1 class="my-2 leading-10 text-4xl font-semibold">{{ $frontmatter.title }}</h1>
+            <h1 class="my-2 leading-10 text-4xl font-semibold">{{ title }}</h1>
             <h2 class="my-2">
                 {{ date.string }} •
                 <span>
-                    Written by <span class="font-semibold">{{ $frontmatter.author }}</span>
+                    Written by <span class="font-semibold">{{ author }}</span>
                 </span>
             </h2>
         </template>
     </Layout>
 </template>
 
-<style>
+<style scoped>
 .image-wrapper {
     @apply flex flex-wrap justify-evenly gap-y-2;
 }
