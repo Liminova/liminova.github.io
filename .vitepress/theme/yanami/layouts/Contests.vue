01df66fd712dba32c0fafec2b5fae2abc2d2b581
Delnegend <kiennguyen19323@gmail.com>
2024-09-02 16:06:29 +0700

    feat(`Contests.vue`): new grid layout

--- .vitepress/theme/yanami/layouts/Contests.vue
+++ .vitepress/theme/yanami/layouts/Contests.vue
@@ -4,45 +4,23 @@
 		<p class="my-6">We aren't very good yet, but we're trying. :3</p>
 		<p class="my-6">All times are in UTC+7 (ICT), as displayed on CTFtime.</p>
 		<hr class="my-8" />
-		<div class="rounded-md border">
-			<table class="w-full caption-bottom text-sm">
-				<thead>
-					<tr class="border-b">
-						<th class="-ml-4 h-12 px-4 text-left align-middle font-medium">
-							Placement
-						</th>
-						<th class="-ml-4 h-12 px-4 text-left align-middle font-medium">Contest</th>
-						<th class="-ml-4 h-12 px-4 text-right align-middle font-medium">
-							CTFtime Rating
-						</th>
-					</tr>
-				</thead>
-				<tbody v-for="contest in data" :key="contest.name">
-					<td class="-ml-4 h-12 w-10 p-4 text-center align-middle">
-						<div class="text-2xl font-semibold">#{{ contest.placement }}</div>
-					</td>
-					<td class="-ml-4 h-12 p-4 text-left align-middle">
-						<div>
-							<div class="text-xl font-semibold">
-								{{ contest.name }}
-							</div>
-							<div>{{ contest.time }}</div>
-						</div>
-					</td>
-					<td class="-ml-4 h-12 px-4 text-right align-middle">
-						<div>
-							<div>
-								<span class="text-xl font-semibold">{{ contest.ctfPoints }}</span>
-								points
-							</div>
-							<div>
-								<span class="text-xl font-semibold">{{ contest.ctfRating }}</span>
-								rating
-							</div>
-						</div>
-					</td>
-				</tbody>
-			</table>
+		<div class="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4">
+			<div v-for="contest in data" :key="contest.time" class="flex size-full flex-col justify-between rounded-md border">
+				<div class="text-balance border-b px-4 py-3 text-center text-lg font-semibold">{{ contest.name }}</div>
+
+				<div class="flex grow items-center justify-evenly border-b px-4 py-5">
+					<div>
+						<span class="mr-1 align-top text-xl">#</span>
+						<span class="text-3xl font-black">{{ contest.placement }}</span>
+					</div>
+					<div>
+						<div v-if="contest.ctfPoints"><span class="font-bold">{{ contest.ctfPoints }}</span> points</div>
+						<div v-if="contest.ctfRating"><span class="font-bold">{{ contest.ctfRating }}</span> rating</div>
+					</div>
+				</div>
+
+				<div class="mx-4 my-1 text-center text-sm">{{ contest.time }}</div>
+			</div>
 		</div>
 		<div v-if="data.length === 0" class="my-4">Nothing here just yet, but we're cooking.</div>
 	</div>
