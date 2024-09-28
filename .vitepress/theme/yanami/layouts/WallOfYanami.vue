92cc52776844e575706deecec549a1e95e639313
j1nxie <rylieeeeexd@gmail.com>
2024-09-28 13:52:21 +0700

    fix: make text smol

--- .vitepress/theme/yanami/layouts/WallOfYanami.vue
+++ .vitepress/theme/yanami/layouts/WallOfYanami.vue
@@ -8,26 +8,26 @@
 		<hr class="my-8" />
 		<figure class="mb-4">
 			<img src="/yanami/s1e5-00.03.30.669.jpg" />
-			<figcaption class="my-2 text-center italic">
+			<figcaption class="my-2 text-center text-sm italic">
 				Episode 5 - Asagumo Chihaya Is Led Astray - 03:30.669
 			</figcaption>
 		</figure>
 		<figure class="mb-4">
 			<img src="/yanami/s1e5-00.19.19.116.jpg" />
-			<figcaption class="my-2 text-center italic">
+			<figcaption class="my-2 text-center text-sm italic">
 				Episode 5 - Asagumo Chihaya Is Led Astray - 19:19.669
 			</figcaption>
 		</figure>
 		<figure class="mb-4">
 			<img src="/yanami/s1e6-00.17.35.222.jpg" />
-			<figcaption class="my-2 text-center italic">
+			<figcaption class="my-2 text-center text-sm italic">
 				Episode 6 - Let Any One of You Who Has Never Been Dumped Be the First to Throw a
 				Stone at the Losing Heroine - 03:30.669
 			</figcaption>
 		</figure>
 		<figure class="mb-4">
 			<img src="/yanami/s1e10-00.09.47.545.jpg" />
-			<figcaption class="my-2 text-center italic">
+			<figcaption class="my-2 text-center text-sm italic">
 				Episode 10 - I Suppose It's a Bit Too Early for Goodbye - 09:47.545
 			</figcaption>
 		</figure>
