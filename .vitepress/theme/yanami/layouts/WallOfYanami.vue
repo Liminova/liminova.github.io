255db6dc5e7200dca60b84cbd0527110db4f110c
j1nxie <rylieeeeexd@gmail.com>
2024-09-28 23:41:45 +0700

    feat: yes.

--- .vitepress/theme/yanami/layouts/WallOfYanami.vue
+++ .vitepress/theme/yanami/layouts/WallOfYanami.vue
@@ -1,35 +1,175 @@
 <template>
-	<div class="mx-auto my-0 max-w-6xl px-8 pb-0 pt-10">
+	<div class="mx-auto my-0 max-w-full px-8 pb-0 pt-10">
 		<h1 class="text-5xl font-semibold leading-10">Wall of Yanami</h1>
 		<p class="my-6">
 			A collection of Anna Yanami's frames from Make Heroine ga Oosugiru! - the namesake of
 			this website internally. Why? Why <span class="italic">not?</span>
 		</p>
 		<hr class="my-8" />
-		<figure class="mb-4">
-			<img src="/yanami/s1e5-00.03.30.669.jpg" />
-			<figcaption class="my-2 text-center text-sm italic">
-				Episode 5 - Asagumo Chihaya Is Led Astray - 03:30.669
-			</figcaption>
-		</figure>
-		<figure class="mb-4">
-			<img src="/yanami/s1e5-00.19.19.116.jpg" />
-			<figcaption class="my-2 text-center text-sm italic">
-				Episode 5 - Asagumo Chihaya Is Led Astray - 19:19.669
-			</figcaption>
-		</figure>
-		<figure class="mb-4">
-			<img src="/yanami/s1e6-00.17.35.222.jpg" />
-			<figcaption class="my-2 text-center text-sm italic">
-				Episode 6 - Let Any One of You Who Has Never Been Dumped Be the First to Throw a
-				Stone at the Losing Heroine - 03:30.669
-			</figcaption>
-		</figure>
-		<figure class="mb-4">
-			<img src="/yanami/s1e10-00.09.47.545.jpg" />
-			<figcaption class="my-2 text-center text-sm italic">
-				Episode 10 - I Suppose It's a Bit Too Early for Goodbye - 09:47.545
-			</figcaption>
-		</figure>
+		<div class="md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3">
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e1-00.03.48.770.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 1 - Professional Childhood Friend Yanami Anna's Style of Losing -
+					03:48.770
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e1-00.18.07.629.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 1 - Professional Childhood Friend Yanami Anna's Style of Losing -
+					18:07.629
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e1-00.19.37.803.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 1 - Professional Childhood Friend Yanami Anna's Style of Losing -
+					19:37.803
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e2-00.14.30.120.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 2 - The Promised Failure for You - 14:30.120
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e2-00.20.37.070.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 2 - The Promised Failure for You - 20:37.070
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e3-00.16.00.835.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 3 - Losing the Battle Before it is Ever Fought - 16:00.835
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e3-00.20.15.464.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 3 - Losing the Battle Before it is Ever Fought - 20:15.464
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.03.38.010.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 03:38.010
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.05.09.643.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 05:09.643
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.21.31.333.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 21:31.333
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.21.37.714.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 21:37.714
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.21.37.881.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 21:37.881
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.22.01.321.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 22:01.321
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e4-00.23.10.015.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 4 - When You Stare into a Losing Heroine, the Losing Heroine Stares Back
+					into You - 23:10.015
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e5-00.03.30.669.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 5 - Asagumo Chihaya Is Led Astray - 03:30.669
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e5-00.13.01.447.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 5 - Asagumo Chihaya Is Led Astray - 13:01.447
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e5-00.15.12.161.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 5 - Asagumo Chihaya Is Led Astray - 15:12:161
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e5-00.19.19.116.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 5 - Asagumo Chihaya Is Led Astray - 19:19.669
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e6-00.17.35.222.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 6 - Let Any One of You Who Has Never Been Dumped Be the First to Throw a
+					Stone at the Losing Heroine - 17:35.222
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e6-00.20.38.238.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 6 - Let Any One of You Who Has Never Been Dumped Be the First to Throw a
+					Stone at the Losing Heroine - 20:38:238
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e6-00.20.46.997.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 6 - Let Any One of You Who Has Never Been Dumped Be the First to Throw a
+					Stone at the Losing Heroine - 20:46.997
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e7-00.03.55.902.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 7 - The Other Side of a Happy Ending - 03:55.902
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e7-00.04.42.407.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 7 - The Other Side of a Happy Ending - 04:42.407
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e7-00.07.59.813.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 7 - The Other Side of a Happy Ending - 07:59.813
+				</figcaption>
+			</figure>
+			<figure class="mb-3 md:mb-0">
+				<img src="/yanami/s1e10-00.09.47.545.jpg" />
+				<figcaption class="my-2 text-center text-sm italic">
+					Episode 10 - I Suppose It's a Bit Too Early for Goodbye - 09:47.545
+				</figcaption>
+			</figure>
+		</div>
 	</div>
 </template>
