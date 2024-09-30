91a633750e2ff4430c474dc8d8240872401fd3f3
Delnegend <kiennguyen19323@gmail.com>
2024-09-30 15:59:34 +0700

    chore(`eslint.config.js`): names for rule groups

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,15 @@ import vue from "eslint-plugin-vue";
 import globals from "globals";
 
 export default [
-	{ ignores: [".vitepress/cache/**/*.*", ".vitepress/dist/**/*.*", ".vitepress/theme/env.d.ts", "*.config.{ts,js,cjs}"] },
-	{ files: ["**/*.{ts,vue,mts}"] },
+	{ name: "yanami/includes",ignores: [".vitepress/cache/**/*.*", ".vitepress/dist/**/*.*", ".vitepress/theme/env.d.ts", "*.config.{ts,js,cjs}"] },
+	{ name: "yanami/excludes", files: ["**/*.{ts,vue,mts}"] },
 
 	...hagemanto({ enablePrettier: true }),
 	...vue.configs["flat/essential"],
 	...tailwind.configs["flat/recommended"],
 
 	{
+		name: "yanami/rules",
 		rules: {
 			"vue/multi-word-component-names": "off",
 			"tailwindcss/no-custom-classname": "off",
@@ -21,6 +22,7 @@ export default [
 
 	// this must be last to override all other configs
 	{
+		name: "yanami/language-options",
 		languageOptions: {
 			globals: globals.browser, parserOptions: {
 				project: true, parser: "@typescript-eslint/parser", extraFileExtensions: [".vue"]
