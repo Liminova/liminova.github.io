857659950134c593860409e85f80de43353f8927
Delnegend <kiennguyen19323@gmail.com>
2024-10-03 01:03:38 +0700

    refactor: merge TagBadge and TagBadges

--- .vitepress/theme/yanami/components/TagBadges.vue
+++ .vitepress/theme/yanami/components/TagBadges.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import TagBadge from "./TagBadge.vue";
-
 defineProps<{
 	tagList: Array<string>;
 }>();
@@ -8,6 +6,12 @@ defineProps<{
 
 <template>
 	<ul class="flex flex-wrap">
-		<TagBadge :key="tag" :tag="tag" v-for="tag in tagList" />
+		<li
+			v-for="tag in tagList"
+			:key="tag"
+			class="mb-1 mr-1 inline rounded-md bg-gray-100 px-2.5 py-1 text-center text-xs dark:text-gray-500"
+		>
+			{{ tag }}
+		</li>
 	</ul>
 </template>
