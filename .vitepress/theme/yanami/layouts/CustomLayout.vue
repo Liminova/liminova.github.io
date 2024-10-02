0710ce66f2f924d14d8157c876dfb131b7702ae1
Delnegend <kiennguyen19323@gmail.com>
2024-10-03 01:02:22 +0700

    feat(`CustomLayout.vue`): one post multi author; use Avatar component

--- .vitepress/theme/yanami/layouts/CustomLayout.vue
+++ .vitepress/theme/yanami/layouts/CustomLayout.vue
@@ -5,14 +5,13 @@
 import { useData, useRoute } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { watch } from "vue";
-import type { ThemeConfig } from "../../../config.mts";
+import type { TeamMember } from "../../../config.mjs";
+import Avatar from "../components/Avatar.vue";
 import TagBadges from "../components/TagBadges.vue";
 import { formatDate } from "../libs";
 
 const { Layout } = DefaultTheme;
 const route = useRoute();
-const data = useData<ThemeConfig>();
-const members = data.theme.value.members;
 
 watch(
 	() => route.path,
@@ -20,6 +19,30 @@ watch(
 		window.goatcounter?.count?.({ path, referrer });
 	}
 );
+
+function getPostAuthors(): Array<TeamMember> {
+	const data = useData();
+	const members = (data.theme.value as { members: Array<TeamMember> }).members;
+	const postAuthors = data.frontmatter.value.author as string | Array<string> | undefined;
+	if (postAuthors === undefined) {
+		throw new Error(`author field in ${data.page.value.filePath} is empty`);
+	}
+
+	switch (true) {
+		case typeof postAuthors === "string":
+			return members.filter((member) => member.name === postAuthors);
+		case typeof postAuthors === "object":
+			return members.reduce((acc: Array<TeamMember>, member: TeamMember) => {
+				if (postAuthors.includes(member.name)) {
+					acc.push(member);
+				}
+
+				return acc;
+			}, []);
+		default:
+			throw new Error(`invalid author type ${typeof postAuthors}`);
+	}
+}
 </script>
 
 <template>
@@ -30,34 +53,16 @@ watch(
 			</h1>
 			<h2 class="my-2">
 				<div class="flex max-w-fit items-center justify-between">
-					<video
-						v-if="
-							members
-								?.filter((member) => member.name === $frontmatter.author)[0]
-								.avatar.endsWith('.mp4')
-						"
-						class="mr-2 inline-block aspect-square w-8 rounded-full"
-						:src="
-							members?.filter((member) => member.name === $frontmatter.author)[0]
-								.avatar
-						"
-						autoplay
-						muted
-						loop
-						playsinline
-					/>
-					<img
-						class="mr-2 inline-block aspect-square w-8 rounded-full"
-						:src="
-							members?.filter((member) => member.name === $frontmatter.author)[0]
-								.avatar
-						"
-						v-else
-					/>
-					<span>
-						<span class="font-semibold">{{ $frontmatter.author }}</span>
-						• {{ formatDate($frontmatter.date).readable }}
-					</span>
+					<div v-for="author in getPostAuthors()" :key="author.name">
+						<Avatar
+							:src="author.avatar"
+							class="mr-2 inline-block aspect-square w-8 rounded-full"
+						/>
+						<span>
+							<span class="font-semibold">{{ author.name }}</span>
+						</span>
+					</div>
+					&nbsp;• {{ formatDate($frontmatter.date).readable }}
 				</div>
 				<TagBadges class="my-3" :tagList="$frontmatter.tags" v-if="$frontmatter.tags" />
 			</h2>
