4f03b8b97676430628d31c7826c3c7f03f3ef551
Delnegend <kiennguyen19323@gmail.com>
2025-10-08 00:30:36 +0700

    build(`.devcontainer`): rm volta, zsh

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# js things
-curl https://get.volta.sh | bash
-export VOLTA_HOME="$HOME/.volta" && export PATH="$VOLTA_HOME/bin:$PATH"
-echo 'export VOLTA_HOME="$HOME/.volta"' >> ~/.zshrc
-echo 'export PATH="$VOLTA_HOME/bin:$PATH"' >> ~/.zshrc
-volta install node@lts pnpm && pnpm config set store-dir ~/.pnpm-store
+SHELL=bash curl -fsSL https://get.pnpm.io/install.sh | bash -
+export PNPM_HOME="/home/liminova/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+pnpm config set store-dir ~/.pnpm-store
+pnpm env use -g lts
