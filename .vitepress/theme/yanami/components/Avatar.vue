74cd2dc6c81f6afd6b81964f8034ecedbcfe3929
Delnegend <kiennguyen19323@gmail.com>
2025-10-08 00:33:42 +0700

    refactor(`Avatar`): `defineProps` w/ TS; fix video on Firefox

--- .vitepress/theme/yanami/components/Avatar.vue
+++ .vitepress/theme/yanami/components/Avatar.vue
@@ -1,27 +1,22 @@
-<!-- Headless Avatar component. Don't include any styles. -->
-
 <script setup lang="ts">
-const props = defineProps({
-	src: {
-		type: String,
-		required: true
-	},
-	class: {
-		type: String,
-		required: false
-	}
-})
+import { cn } from '../libs/classname'
+
+const props = defineProps<{
+	src: string
+	class?: string
+	alt?: string
+}>()
 </script>
 
 <template>
 	<video
 		v-if="props.src.endsWith('mp4')"
 		:src="props.src"
-		:class="props.class"
+		:class="cn('!h-full', props.class)"
 		autoplay
 		muted
 		loop
 		playsinline
 	/>
-	<img v-else :class="props.class" :src="props.src" />
+	<img v-else :class="props.class" :src="props.src" :alt="props.alt" />
 </template>
