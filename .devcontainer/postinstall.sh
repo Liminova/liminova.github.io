2cb87405fce553697c5dd9854941592c308933cf
Delnegend <kiennguyen19323@gmail.com>
2026-06-13 20:54:21 +0700

    fix: `postinstall` script set `pnpm` path

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
 
 SHELL=bash curl -fsSL https://get.pnpm.io/install.sh | bash -
+
 export PNPM_HOME="/home/liminova/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
+export PATH="$PNPM_HOME:$PNPM_HOME/bin:$PATH"
+pnpm setup 2>/dev/null || true
 pnpm config set store-dir ~/.pnpm-store
 pnpm env use -g lts
